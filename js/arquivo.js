9b833230525b145013311432a076701954806d33
KenzoFukushima <20241ctb0100081@estudantes.ifpr.edu.br>
2025-07-01 12:25:33 -0300

    Aula de JavaScript 01/07

---
+++ js/arquivo.js
@@ -0,0 +1,26 @@
+var global = "variavel global";
+console.log(global);
+
+
+function exemplo2() {
+    var local = "variavel local";
+    console.log(local);
+}
+exemplo2();
+// console.log(local);
+
+let x = 10;
+if(x > 5) {
+    let y = 25;
+    console.log("valor de y : ", y);
+}
+// console.log(x);
+// console.log(y);
+
+const _constante = "text";
+_constante = "Outro texto";
+console.log(_constante);
+
+function saudacao() {
+    alert("Bem-Vindo ao nosso site 3");
+}
