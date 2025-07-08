5a62c539d4fa61c7d3add04af9e7dd17be5c36f4
= <=>
2025-07-08 12:28:41 -0300

    Aula dia 08/07

--- js/arquivo.js
+++ js/arquivo.js
@@ -1,3 +1,11 @@
+let num1 = Number(prompt("1. Digite o primeiro numero: "));
+
+let num2 = Number(prompt("2. Digite o sugundo numero: "));
+
+alert("Rusultado da soma: " + (num1+num2));
+
+
+
 var global = "variavel global";
 console.log(global);
 
