df086932ca1583226a7d4a260e2cda8c3c822c31
KenzoFukushima <20241ctb0100081@estudantes.ifpr.edu.br>
2025-07-14 20:48:27 -0300

    adicao de exercicio 4,5,6

--- exercicios/script.js
+++ exercicios/script.js
@@ -19,4 +19,37 @@ function verificar(){
 
 function contadorLetra(){
     let texto = document.getElementById("texto").value;
+    let totalCaracter = texto.length;
+
+    document.getElementById("resultado3").innerText = "Total de caracteres: " + totalCaracter;
+}
+
+function verificarParImpar() {
+    let num = Number(document.getElementById("numParImpar").value);
+
+    if(num % 2 == 0) {
+        document.getElementById("resultado4").innerText = "É Par";
+    } else {
+        document.getElementById("resultado4").innerText = "É Impar";
+    }
+}
+
+function convercaoMaiuscula() {
+    let text = document.getElementById("textAlta").value;
+    let textoConvertido = text.toUpperCase();
+
+    document.getElementById("resultado5").innerText = "Convertido: " + textoConvertido;
+}
+
+function verificarMaior() {
+    let n1 = Number(document.getElementById("num6_1").value);
+    let n2 = Number(document.getElementById("num6_2").value);
+
+    if(n1 > n2) {
+        document.getElementById("resultado6").innerText = "Maior: " + n1;
+    } else if(n2 > n1) {
+        document.getElementById("resultado6").innerText = "Maior: " + n2;
+    } else {
+        document.getElementById("resultado6").innerText = "São iguais";
+    }
 }
